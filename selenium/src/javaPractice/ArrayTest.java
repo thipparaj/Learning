b7fed89bf4c9df19496a340df4f6ed3841bbbd8d
think <thipparaj@gmail.com>
2019-07-22 01:22:58 +0530

    creating a array class...

---
+++ selenium/src/javaPractice/ArrayTest.java
@@ -0,0 +1,13 @@
+package javaPractice;
+
+public class ArrayTest {
+
+	/*Creating the array
+	 * print the array
+	 * using for loop and for each methods*/
+	public static void main(String[] args) {
+		
+
+	}
+
+}
